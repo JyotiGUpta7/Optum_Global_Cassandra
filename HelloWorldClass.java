838f562951a2ade610e7efc87b08cc8f33553107
Arnab Chakraborty <111301975+toarnabtrainer@users.noreply.github.com>
2022-11-17 00:01:23 -0800

    Day-2

---
+++ HelloWorldClass.java
@@ -0,0 +1,8 @@
+package com.java.demo;
+
+public class HelloWorldClass {
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!");
+		System.out.println("End of the program...");
+	}
+}
